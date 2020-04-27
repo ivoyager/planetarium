22879a9faf0dc1b2c59b9b83b3c2c52e8222a96a
Charlie Whitfield <charliewhitfield@zoho.com>
2020-04-26 22:54:38 -0400

    Update ivoyager & planetarium.gd

--- planetarium/planetarium.gd
+++ planetarium/planetarium.gd
@@ -1,6 +1,6 @@
 # planetarium.gd
-# This file is part of I, Voyager
-# https://ivoyager.dev
+# This file is part of I, Voyager (https://ivoyager.dev)
+# *****************************************************************************
 # Copyright (c) 2017-2020 Charlie Whitfield
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
