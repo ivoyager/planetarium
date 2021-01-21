71e86ea99f815a95e89381db291be606a7a3d521
Charlie Whitfield <charliewhitfield@zoho.com>
2021-01-21 16:04:06 -0500

    NavPanel has ContainerDraggable mod

--- planetarium/gui/pltm_nav_panel.gd
+++ planetarium/gui/pltm_nav_panel.gd
@@ -23,12 +23,12 @@ onready var _under_moons_spacer: Control = find_node("UnderMoonsSpacer")
 var _under_moons_spacer_sizes := [55.0, 66.0, 77.0]
 
 func _ready():
-	$ContainerDynamic.default_sizes = [
+	$ContainerDraggable.default_sizes = [
 		Vector2(435.0, 278.0), # GUI_SMALL
 		Vector2(575.0, 336.0), # GUI_MEDIUM
 		Vector2(712.0, 400.0), # GUI_LARGE
 	]
-	$ContainerDynamic.max_default_screen_proportions = Vector2(0.55, 0.45)
+	$ContainerDraggable.max_default_screen_proportions = Vector2(0.55, 0.45)
 	Global.connect("gui_refresh_requested", self, "_resize")
 	Global.connect("setting_changed", self, "_settings_listener")
 
