94dd75b732c785413419899de79d14b95db0d771
Charlie Whitfield <charliewhitfield@zoho.com>
2021-02-07 17:13:08 -0500

    Update licence headers

--- planetarium/gui/pltm_menu_panel.gd
+++ planetarium/gui/pltm_menu_panel.gd
@@ -3,7 +3,7 @@
 # https://ivoyager.dev
 # *****************************************************************************
 # Copyright (c) 2017-2021 Charlie Whitfield
-# "I, Voyager" is a registered trademark of Charlie Whitfield
+# I, Voyager is a registered trademark of Charlie Whitfield
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
