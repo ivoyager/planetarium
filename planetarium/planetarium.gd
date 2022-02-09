a54587884183d9f49725c7065b7e11208e9197bb
Charlie Whitfield <charliewhitfield@zoho.com>
2022-02-09 14:43:31 -0500

    Update ivoyager w/ class changes

--- planetarium/planetarium.gd
+++ planetarium/planetarium.gd
@@ -30,7 +30,7 @@ extends Reference
 
 const EXTENSION_NAME := "Planetarium"
 const EXTENSION_VERSION := "0.0.13-DEV"
-const EXTENSION_VERSION_YMD := 20220126
+const EXTENSION_VERSION_YMD := 20220209
 const DEBUG_BUILD := "" # ymd + this displayed when version ends with "-DEV"
 
 const USE_THREADS := true # false for debugging
