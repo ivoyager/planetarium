0fda88e5d583692a17ea9a6db0204faff73a4f31
Charlie Whitfield <charliewhitfield@zoho.com>
2023-02-23 12:11:02 -0500

    Finish project restructure

--- planetarium/gui/planetarium_gui.gd
+++ planetarium/gui/planetarium_gui.gd
@@ -22,18 +22,9 @@ extends Control
 const SCENE := "res://planetarium/gui/planetarium_gui.tscn"
 
 # Scenes instanced by IVProjectBuilder need SCENE constant above.
-#
-# An IVSelectionManager instance manages our current selection. To find this
-# instanace, various GUI widgets search up their ancestor tree for the first
-# node that has a "selection_manager" member.
-
-var selection_manager: IVSelectionManager
-
-onready var _SelectionManager_: Script = IVGlobal.script_classes._SelectionManager_
 
 
 func _project_init() -> void:
-	IVGlobal.connect("project_builder_finished", self, "_on_project_builder_finished")
 	IVGlobal.connect("system_tree_built_or_loaded", self, "_on_system_tree_built_or_loaded")
 	IVGlobal.connect("simulator_exited", self, "_on_simulator_exited")
 	hide()
@@ -41,7 +32,7 @@ func _project_init() -> void:
 
 func _ready():
 	var style_box := StyleBoxFlat.new()
-	style_box.bg_color = Color(1.0, 1.0, 1.0, 0.05) # almost transparent
+	style_box.bg_color = Color(1.0, 1.0, 1.0, 0.1) # almost transparent
 	for child in get_children():
 		var panel_container := child as PanelContainer
 		if !panel_container:
@@ -51,14 +42,7 @@ func _ready():
 	$ControlPanel.connect("time_set_requested", $TimeSetPopup, "popup")
 
 
-func _on_project_builder_finished() -> void:
-	theme = IVGlobal.themes.main
-
-
-func _on_system_tree_built_or_loaded(is_new_game: bool) -> void:
-	if is_new_game:
-		selection_manager = _SelectionManager_.new()
-		add_child(selection_manager)
+func _on_system_tree_built_or_loaded(_is_new_game: bool) -> void:
 	show()
 
 
