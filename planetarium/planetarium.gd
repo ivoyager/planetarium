196287b4ce5f4d278eee05cae0601a6f42025051
Charlie Whitfield <charliewhitfield@zoho.com>
2023-09-18 15:19:01 -0400

    Update ivoyager & planetarium.gd

--- planetarium/planetarium.gd
+++ planetarium/planetarium.gd
@@ -31,9 +31,9 @@ const EXTENSION_NAME := "Planetarium"
 const EXTENSION_VERSION := "0.0.16"
 const EXTENSION_BUILD := ""
 const EXTENSION_STATE := "dev" # 'dev', 'alpha', 'beta', 'rc', ''
-const EXTENSION_YMD := 20230917 # displayed if EXTENSION_STATE = 'dev'
+const EXTENSION_YMD := 20230918 # displayed if EXTENSION_STATE = 'dev'
 
-const USE_THREADS := false # set false for debugging
+const USE_THREADS := true # set false for debugging
 const NO_THREADS_IF_HTML5 := true # overrides above
 
 const VERBOSE_GLOBAL_SIGNALS := false
